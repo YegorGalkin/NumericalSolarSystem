878663dffaafce75333640a86e773008a90d7aee
GadS06 <gads06121994@yandex.ru>
2015-05-07 02:31:47 +0300

    Сделал кривые. Нужна небольшая доработка, но уже неплохо!

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -9,6 +9,8 @@ public class Controller : MonoBehaviour
 	public double timeSpeed = 1;
 	public double timePerStep;
 	public int stepNum;
+	public float trajectoryStep = 100;
+	public int trajectoryStepNum = 10;
 
 	private int currentStep;
 	private int currentStep_model1;
@@ -20,43 +22,50 @@ public class Controller : MonoBehaviour
 		currentStep_model1 = 0;
 		storage1 = new Storages.VectorMegaStorage(timePerStep, stepNum);
 		storage1.Initialise();
+		for (int k = 0; k < planets.Length; k++)
+		{
+			planets[k].GetComponent<LineRenderer>().SetVertexCount(2 * trajectoryStepNum + 3);
+		}
 	}
 
 	void FixedUpdate()
 	{
 		// Двигаем время
 		time += timeSpeed * Time.deltaTime;
-		//currentStep = (int)(time / timePerStep);
 
-		//while (currentStep > currentStep_model1)
-		//{
-		//	model1.NextStep();
-		//	currentStep_model1++;
-		//}
+		// Рисуем траекторию
+		int j = 0;
+		var lastV = storage1.GetCoordinatesAtTime(0, 0);
+		for (int i = -trajectoryStepNum; i < trajectoryStepNum; i++)
+		{
+			try
+			{
+				var v = storage1.GetCoordinatesAtTime((float)time + i * trajectoryStep, 0);
+				for (int k = 0; k < planets.Length; k++)
+				{
+					planets[k].GetComponent<LineRenderer>().SetPosition(j, new Vector3(v[k * 3], v[k * 3 + 1], v[k * 3 + 2]));
+					lastV = v;
+				}
+				j++;
+			}
+			catch (System.Exception ex) { }
+			finally { }
+		}
+		while (j < 2 * trajectoryStepNum + 3)
+		{
+			for (int k = 0; k < planets.Length; k++)
+			{
+				planets[k].GetComponent<LineRenderer>().SetPosition(j, new Vector3(lastV[k * 3], lastV[k * 3 + 1], lastV[k * 3 + 2]));
+			}
+			j++;
+		}
 
-		var v = storage1.GetCoordinatesAtTime((float)time, 0);
-		var v100 = storage1.GetCoordinatesAtTime((float)time + 1000, 0);
-
+		// Выставляем координаты
+		var f = storage1.GetCoordinatesAtTime((float)time, 0);
 		int l = planets.Length;
 		for (int i = 0; i < l; i++)
 		{
-			//planets[i].GetComponent<Transform>().position = new Vector3(i * Mathf.Cos((float)((l - i) * time)), i * Mathf.Sin((float)((l - i) * time)), 0);
-			planets[i].GetComponent<Transform>().position = new Vector3(v[i * 3], v[i * 3 + 1], v[i * 3 + 2]);
-			planets[i].GetComponent<LineRenderer>().SetPosition(0, new Vector3(v[i * 3], v[i * 3 + 1], v[i * 3 + 2]));
-			planets[i].GetComponent<LineRenderer>().SetPosition(1, new Vector3(v100[i * 3], v100[i * 3 + 1], v100[i * 3 + 2]));
+			planets[i].GetComponent<Transform>().position = new Vector3(f[i * 3], f[i * 3 + 1], f[i * 3 + 2]);
 		}
-	}
-
-	public Vector3 GetPlanetCoords(GameObject arg_planet, double arg_time)
-	{
-		for(int i = 0; i < planets.Length; i++)
-		{
-			if(planets[i] = arg_planet)
-			{
-				var v = storage1.GetCoordinatesAtTime((float)arg_time, 0);
-				return new Vector3(v[i * 3], v[i * 3 + 1], v[i * 3 + 2]);
-			}
-		}
-		throw new System.Exception("There is no such planet.");
 	}
 }
