9bc42688bc91fb97618d697d76c43805ee10da61
GadS06 <gads06121994@yandex.ru>
2015-05-07 09:04:26 +0300

    Добавил второй метод.

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class Controller : MonoBehaviour
 {
 	public GameObject[] planets;
+	public GameObject[] planets_1;
 	public double time = 0;
 	public double timeSpeed = 1;
 	public double timePerStep;
@@ -25,6 +26,8 @@ public class Controller : MonoBehaviour
 		for (int k = 0; k < planets.Length; k++)
 		{
 			planets[k].GetComponent<LineRenderer>().SetVertexCount(2 * trajectoryStepNum + 3);
+			planets_1[k].GetComponent<LineRenderer>().SetVertexCount(2 * trajectoryStepNum + 3);
+			planets_1[k].GetComponent<LineRenderer>().SetColors(Color.red, Color.red);
 		}
 	}
 
@@ -32,19 +35,24 @@ public class Controller : MonoBehaviour
 	{
 		// Двигаем время
 		time += timeSpeed * Time.deltaTime;
+		if (time < 0) time = 0;
 
 		// Рисуем траекторию
 		int j = 0;
 		var lastV = storage1.GetCoordinatesAtTime(0, 0);
+		var lastV_1 = storage1.GetCoordinatesAtTime(0, 1);
 		for (int i = -trajectoryStepNum; i < trajectoryStepNum; i++)
 		{
 			try
 			{
 				var v = storage1.GetCoordinatesAtTime((float)time + i * trajectoryStep, 0);
+				var v_1 = storage1.GetCoordinatesAtTime((float)time + i * trajectoryStep, 1);
 				for (int k = 0; k < planets.Length; k++)
 				{
 					planets[k].GetComponent<LineRenderer>().SetPosition(j, new Vector3(v[k * 3], v[k * 3 + 1], v[k * 3 + 2]));
+					planets_1[k].GetComponent<LineRenderer>().SetPosition(j, new Vector3(v_1[k * 3], v_1[k * 3 + 1], v_1[k * 3 + 2]));
 					lastV = v;
+					lastV_1 = v_1;
 				}
 				j++;
 			}
@@ -56,16 +64,19 @@ public class Controller : MonoBehaviour
 			for (int k = 0; k < planets.Length; k++)
 			{
 				planets[k].GetComponent<LineRenderer>().SetPosition(j, new Vector3(lastV[k * 3], lastV[k * 3 + 1], lastV[k * 3 + 2]));
+				planets_1[k].GetComponent<LineRenderer>().SetPosition(j, new Vector3(lastV_1[k * 3], lastV_1[k * 3 + 1], lastV_1[k * 3 + 2]));
 			}
 			j++;
 		}
 
 		// Выставляем координаты
 		var f = storage1.GetCoordinatesAtTime((float)time, 0);
+		var f_1 = storage1.GetCoordinatesAtTime((float)time, 1);
 		int l = planets.Length;
 		for (int i = 0; i < l; i++)
 		{
 			planets[i].GetComponent<Transform>().position = new Vector3(f[i * 3], f[i * 3 + 1], f[i * 3 + 2]);
+			planets_1[i].GetComponent<Transform>().position = new Vector3(f_1[i * 3], f_1[i * 3 + 1], f_1[i * 3 + 2]);
 		}
 	}
 }
